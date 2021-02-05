d9ba7bd564d6dabc19706aa89f1b25c6d9cc92ad
rockshellS <spicer.rachelle@gmail.com>
2021-02-05 10:54:31 -0600

    commit tree 1cd9d1d810f559b6fedb404a1be8d4573f222ca8

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+// let userData = {
+//     name: 'Dj James',
+//     email: 'DjJames@gmail.com'
+// };
+
+let configObj = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+
+    body: JSON.stringify({
+        name: 'Dj James',
+        email: 'DjJames@gmail.com'
+    })
+};
+
+fetch('http://localhost:3000/users')
+.then(resp => resp.json()) 
+.then(user => user)
+}
+
+submitData(name, email)
